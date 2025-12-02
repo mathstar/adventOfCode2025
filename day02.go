4cfbf05093b50e9b0fbdae86818334cc7c325053
Mathias Staricka <mstaricka@gmail.com>
2025-12-01 22:19:07 -0800

    unify common logic

--- day02.go
+++ day02.go
@@ -16,40 +16,13 @@ func (_ day2) part1(input string) string {
 	results := make([]chan int, len(ranges))
 	for i, r := range ranges {
 		results[i] = make(chan int)
-		go determineInvalidCount(r, results[i])
+		go determineInvalidCount(r, checkValiditySimple, results[i])
 	}
 	var sum int
 	for _, r := range results {
 		sum += <-r
 	}
 	return strconv.Itoa(sum)
-}
-
-func determineInvalidCount(r string, ch chan int) {
-	split := strings.Split(r, "-")
-	startInc, err := strconv.Atoi(split[0])
-	if err != nil {
-		panic(err)
-	}
-	endInc, err := strconv.Atoi(split[1])
-	if err != nil {
-		panic(err)
-	}
-	ch <- invalidSum(startInc, endInc)
-}
-
-func invalidSum(startInc, endInc int) int {
-	var sum int
-	for i := startInc; i <= endInc; i++ {
-		s := strconv.Itoa(i)
-		if len(s)%2 == 1 {
-			continue
-		}
-		if s[0:len(s)/2] == s[len(s)/2:] {
-			sum += i
-		}
-	}
-	return sum
 }
 
 func (_ day2) part2(input string) string {
@@ -57,7 +30,7 @@ func (_ day2) part2(input string) string {
 	results := make([]chan int, len(ranges))
 	for i, r := range ranges {
 		results[i] = make(chan int)
-		go determineInvalidCount2(r, results[i])
+		go determineInvalidCount(r, checkValidityComplex, results[i])
 	}
 	var sum int
 	for _, r := range results {
@@ -66,7 +39,7 @@ func (_ day2) part2(input string) string {
 	return strconv.Itoa(sum)
 }
 
-func determineInvalidCount2(r string, ch chan int) {
+func determineInvalidCount(r string, validityFunc func(int) bool, ch chan int) {
 	split := strings.Split(r, "-")
 	startInc, err := strconv.Atoi(split[0])
 	if err != nil {
@@ -76,30 +49,46 @@ func determineInvalidCount2(r string, ch chan int) {
 	if err != nil {
 		panic(err)
 	}
-	ch <- invalidSum2(startInc, endInc)
+	ch <- invalidSum(startInc, endInc, validityFunc)
 }
 
-func invalidSum2(startInc, endInc int) int {
+func invalidSum(startInc, endInc int, validityFunc func(int) bool) int {
 	var sum int
-numberLoop:
 	for i := startInc; i <= endInc; i++ {
-		s := strconv.Itoa(i)
-	splitLengthLoop:
-		for splitLength := 1; splitLength <= len(s)/2; splitLength++ {
-			if len(s)%splitLength == 0 {
-				var splits []string
-				for j := 0; j < len(s); j += splitLength {
-					splits = append(splits, s[j:j+splitLength])
-				}
-				for j := 1; j < len(splits); j++ {
-					if splits[0] != splits[j] {
-						continue splitLengthLoop
-					}
-				}
-				sum += i
-				continue numberLoop
-			}
+		if validityFunc(i) {
+			sum += i
 		}
 	}
 	return sum
+}
+
+func checkValiditySimple(i int) bool {
+	s := strconv.Itoa(i)
+	if len(s)%2 == 1 {
+		return false
+	}
+	if s[0:len(s)/2] == s[len(s)/2:] {
+		return true
+	}
+	return false
+}
+
+func checkValidityComplex(i int) bool {
+	s := strconv.Itoa(i)
+splitLengthLoop:
+	for splitLength := 1; splitLength <= len(s)/2; splitLength++ {
+		if len(s)%splitLength == 0 {
+			var splits []string
+			for j := 0; j < len(s); j += splitLength {
+				splits = append(splits, s[j:j+splitLength])
+			}
+			for j := 1; j < len(splits); j++ {
+				if splits[0] != splits[j] {
+					continue splitLengthLoop
+				}
+			}
+			return true
+		}
+	}
+	return false
 }
