3bf63feb559c4dd0d51a6e8b9aded40462c896a9
Mathias Staricka <mstaricka@gmail.com>
2025-12-01 21:59:02 -0800

    switch back to plain ints

--- day02.go
+++ day02.go
@@ -13,19 +13,19 @@ func init() {
 
 func (_ day2) part1(input string) string {
 	ranges := strings.Split(strings.TrimSpace(input), ",")
-	results := make([]chan int64, len(ranges))
+	results := make([]chan int, len(ranges))
 	for i, r := range ranges {
-		results[i] = make(chan int64)
+		results[i] = make(chan int)
 		go determineInvalidCount(r, results[i])
 	}
-	var sum int64
+	var sum int
 	for _, r := range results {
 		sum += <-r
 	}
-	return strconv.FormatInt(sum, 10)
+	return strconv.Itoa(sum)
 }
 
-func determineInvalidCount(r string, ch chan int64) {
+func determineInvalidCount(r string, ch chan int) {
 	split := strings.Split(r, "-")
 	startInc, err := strconv.Atoi(split[0])
 	if err != nil {
@@ -38,15 +38,15 @@ func determineInvalidCount(r string, ch chan int64) {
 	ch <- invalidSum(startInc, endInc)
 }
 
-func invalidSum(startInc, endInc int) int64 {
-	var sum int64
+func invalidSum(startInc, endInc int) int {
+	var sum int
 	for i := startInc; i <= endInc; i++ {
 		s := strconv.Itoa(i)
 		if len(s)%2 == 1 {
 			continue
 		}
 		if s[0:len(s)/2] == s[len(s)/2:] {
-			sum += int64(i)
+			sum += i
 		}
 	}
 	return sum
@@ -54,19 +54,19 @@ func invalidSum(startInc, endInc int) int64 {
 
 func (_ day2) part2(input string) string {
 	ranges := strings.Split(strings.TrimSpace(input), ",")
-	results := make([]chan int64, len(ranges))
+	results := make([]chan int, len(ranges))
 	for i, r := range ranges {
-		results[i] = make(chan int64)
+		results[i] = make(chan int)
 		go determineInvalidCount2(r, results[i])
 	}
-	var sum int64
+	var sum int
 	for _, r := range results {
 		sum += <-r
 	}
-	return strconv.FormatInt(sum, 10)
+	return strconv.Itoa(sum)
 }
 
-func determineInvalidCount2(r string, ch chan int64) {
+func determineInvalidCount2(r string, ch chan int) {
 	split := strings.Split(r, "-")
 	startInc, err := strconv.Atoi(split[0])
 	if err != nil {
@@ -79,8 +79,8 @@ func determineInvalidCount2(r string, ch chan int64) {
 	ch <- invalidSum2(startInc, endInc)
 }
 
-func invalidSum2(startInc, endInc int) int64 {
-	var sum int64
+func invalidSum2(startInc, endInc int) int {
+	var sum int
 numberLoop:
 	for i := startInc; i <= endInc; i++ {
 		s := strconv.Itoa(i)
@@ -96,7 +96,7 @@ numberLoop:
 						continue splitLengthLoop
 					}
 				}
-				sum += int64(i)
+				sum += i
 				continue numberLoop
 			}
 		}
