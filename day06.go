221c76bd44d5d9a5c2fd8fe4bc751ec3ba3a8b8e
Mathias Staricka <mstaricka@gmail.com>
2025-12-05 15:50:26 -0800

    prep day 6

---
+++ day06.go
@@ -0,0 +1,15 @@
+package main
+
+type day6 struct{}
+
+func init() {
+	registerDay(6, day6{})
+}
+
+func (day6) part1(input string) string {
+	return ""
+}
+
+func (day6) part2(input string) string {
+	return ""
+}
---
+++ day06_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestDay6Part1(t *testing.T) {
+	input := ``
+	want := ""
+	assertPart1(day6{}, input, want, t)
+}
+
+func TestDay6Part2(t *testing.T) {
+	input := ``
+	want := ""
+	assertPart2(day6{}, input, want, t)
+}
