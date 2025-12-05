3f018d68e14eaf0283c91346d50f8092c341d050
Mathias Staricka <mstaricka@gmail.com>
2025-12-05 14:48:27 -0800

    day 4

--- day04.go
+++ day04.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 type day4 struct{}
 
 func init() {
@@ -7,9 +12,85 @@ func init() {
 }
 
 func (_ day4) part1(input string) string {
-	return ""
+	grid := parseGrid(input)
+
+	var movable int
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] && neighborCount(&grid, i, j) < 4 {
+				movable++
+			}
+		}
+	}
+
+	return strconv.Itoa(movable)
 }
 
 func (_ day4) part2(input string) string {
-	return ""
+	grid := parseGrid(input)
+
+	var movable int
+	for removed := true; removed; {
+		removed = false
+		nextGrid := make([][]bool, len(grid))
+		for i := range grid {
+			nextGrid[i] = make([]bool, len(grid[i]))
+			for j := range grid[i] {
+				nextGrid[i][j] = grid[i][j]
+				if grid[i][j] && neighborCount(&grid, i, j) < 4 {
+					removed = true
+					movable++
+					nextGrid[i][j] = false
+				}
+			}
+		}
+		grid = nextGrid
+	}
+
+	return strconv.Itoa(movable)
+}
+
+func parseGrid(input string) [][]bool {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	grid := make([][]bool, len(lines))
+	for i, line := range lines {
+		trimmed := strings.TrimSpace(line)
+		grid[i] = make([]bool, len(trimmed))
+		for j, char := range trimmed {
+			grid[i][j] = char == '@'
+		}
+	}
+	return grid
+}
+
+func neighborCount(grid *[][]bool, i, j int) int {
+	var count int
+	if i > 0 && j > 0 && (*grid)[i-1][j-1] {
+		count++
+	}
+	if i > 0 && (*grid)[i-1][j] {
+		count++
+	}
+	if i > 0 && j < len((*grid)[i-1])-1 && (*grid)[i-1][j+1] {
+		count++
+	}
+
+	if j > 0 && (*grid)[i][j-1] {
+		count++
+	}
+	if j < len((*grid)[i])-1 && (*grid)[i][j+1] {
+		count++
+	}
+
+	if i < len(*grid)-1 && j > 0 && (*grid)[i+1][j-1] {
+		count++
+	}
+	if i < len(*grid)-1 && (*grid)[i+1][j] {
+		count++
+	}
+	if i < len(*grid)-1 && j < len((*grid)[i+1])-1 && (*grid)[i+1][j+1] {
+		count++
+	}
+
+	return count
 }
--- day04_test.go
+++ day04_test.go
@@ -3,13 +3,31 @@ package main
 import "testing"
 
 func TestDay4Part1(t *testing.T) {
-	input := ``
-	want := ""
+	input := `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+	want := "13"
 	assertPart1(day4{}, input, want, t)
 }
 
 func TestDay4Part2(t *testing.T) {
-	input := ``
-	want := ""
+	input := `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+	want := "43"
 	assertPart2(day4{}, input, want, t)
 }
