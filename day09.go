6208b433e8cf5c340dac765a11727efe767cd774
Mathias Staricka <mstaricka@gmail.com>
2025-12-18 16:06:14 -0800

    day 9 part 1

--- day09.go
+++ day09.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 type day9 struct{}
 
 func init() {
@@ -7,9 +12,46 @@ func init() {
 }
 
 func (d day9) part1(input string) string {
-	return ""
+	m := d.findMaxRectangle(d.parseInput(input))
+	return strconv.Itoa(m)
 }
 
 func (d day9) part2(input string) string {
 	return ""
+}
+
+func (day9) parseInput(input string) [][]int {
+	var pairs [][]int
+	for _, line := range strings.Split(input, "\n") {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		split := strings.Split(line, ",")
+		a, err := strconv.Atoi(split[0])
+		if err != nil {
+			panic(err)
+		}
+		b, err := strconv.Atoi(split[1])
+		if err != nil {
+			panic(err)
+		}
+		pairs = append(pairs, []int{a, b})
+	}
+	return pairs
+}
+
+func (day9) findMaxRectangle(pairs [][]int) int {
+	var m int
+	for i := range pairs {
+		for j := i + 1; j < len(pairs); j++ {
+			size := pairs[i][0] - pairs[j][0] + 1
+			size *= pairs[i][1] - pairs[j][1] + 1
+			if size < 0 {
+				size *= -1
+			}
+			m = max(m, size)
+		}
+	}
+	return m
 }
--- day09_test.go
+++ day09_test.go
@@ -3,8 +3,15 @@ package main
 import "testing"
 
 func TestDay9Part1(t *testing.T) {
-	input := ``
-	want := ""
+	input := `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`
+	want := "50"
 	assertPart1(day9{}, input, want, t)
 }
 
